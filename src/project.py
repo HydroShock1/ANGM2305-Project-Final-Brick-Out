53ef9bc036ac6f3604c6a6f273a96162a53938b7
Paul Escobar Jacobs <paul.101601@gmail.com>
2023-12-02 21:29:15 -0600

    Organized what I had so far into classes.

--- src/project.py
+++ src/project.py
@@ -4,71 +4,74 @@
 # Import required library
 import turtle
 
-# Create screen
-sc = turtle.Screen()
-sc.title("Extreme Brick Out")
-sc.bgcolor("white")
-sc.setup(width=600, height=900)
+class GameScreen:
+    def __init__(self):
+        self.screen = turtle.Screen()
+        self.screen.title("Extreme Brick Out")
+        self.screen.bgcolor("white")
+        self.screen.setup(width=600, height=900)
 
-# The bouncing ball
-hit_ball = turtle.Turtle()
-hit_ball.speed(40)
-hit_ball.shape("circle")
-hit_ball.color("blue")
-hit_ball.penup()
-hit_ball.goto(0, 0)
-hit_ball.dx = 5
-hit_ball.dy = -5
+class Ball:
+    def __init__(self):
+        self.ball = turtle.Turtle()
+        self.ball.speed(40)
+        self.ball.shape("circle")
+        self.ball.color("blue")
+        self.ball.penup()
+        self.ball.goto(0, 0)
+        self.ball.dx = 5
+        self.ball.dy = -5
 
-# paddle bar to keep battle from hitting the bottom
-paddle_bar = turtle.Turtle()
-paddle_bar.speed(0)
-paddle_bar.shape("square")
-paddle_bar.color("black")
-paddle_bar.shapesize(stretch_wid=2, stretch_len=6)
-paddle_bar.penup()
-paddle_bar.goto(0, -340)
+class PaddleBar:
+    def __init__(self):
+        self.paddle = turtle.Turtle()
+        self.paddle.speed(0)
+        self.paddle.shape("square")
+        self.paddle.color("black")
+        self.paddle.shapesize(stretch_wid=2, stretch_len=6)
+        self.paddle.penup()
+        self.paddle.goto(0, -340)
 
+    def move_right(self):
+        x = self.paddle.xcor()
+        x += 20
+        self.paddle.setx(x)
 
-# Moving the paddle left and right
-def paddleright():
-    x = paddle_bar.xcor()
-    x += 20
-    paddle_bar.setx(x)
- 
- 
-def paddleleft():
-    x = paddle_bar.xcor()
-    x -= 20
-    paddle_bar.setx(x)
-	
+    def move_left(self):
+        x = self.paddle.xcor()
+        x -= 20
+        self.paddle.setx(x)
+
+# Instances of the classes
+game_screen = GameScreen()
+ball = Ball()
+paddle = PaddleBar()
+
 # Keyboard inputs
-sc.listen()
-sc.onkeypress(paddleright, "Right")
-sc.onkeypress(paddleleft, "Left")
- 
+game_screen.screen.listen()
+game_screen.screen.onkeypress(paddle.move_right, "Right")
+game_screen.screen.onkeypress(paddle.move_left, "Left")
+
 # The interactivity between the ball and the borders
 while True:
-	sc.update()
+    game_screen.screen.update()
 
-	hit_ball.setx(hit_ball.xcor()+hit_ball.dx)
-	hit_ball.sety(hit_ball.ycor()+hit_ball.dy)
-	
-	# Bouncing off borders
-	if hit_ball.ycor() > 445:
-		hit_ball.sety(445)
-		hit_ball.dy *= -1
+    ball.ball.setx(ball.ball.xcor() + ball.ball.dx)
+    ball.ball.sety(ball.ball.ycor() + ball.ball.dy)
 
-	if hit_ball.ycor() < -445:
-		hit_ball.goto(0, 0)
-		hit_ball.dy *= -1
-		
-	if hit_ball.xcor() > 280:
-		hit_ball.setx(280)
-		hit_ball.dx *= -1
+    # Bouncing off borders
+    if ball.ball.ycor() > 445:
+        ball.ball.sety(445)
+        ball.ball.dy *= -1
 
-	if hit_ball.xcor() < -280:
-		hit_ball.setx(-280)
-		hit_ball.dx *= -1
+    if ball.ball.ycor() < -445:
+        ball.ball.goto(0, 0)
+        ball.ball.dy *= -1
 
+    if ball.ball.xcor() > 280:
+        ball.ball.setx(280)
+        ball.ball.dx *= -1
 
+    if ball.ball.xcor() < -280:
+        ball.ball.setx(-280)
+        ball.ball.dx *= -1
